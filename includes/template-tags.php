bbf0b03296ed3cd8086c33da3851b8dfacee7024
RevaARizky <revaathallah86@gmail.com>
2024-05-21 14:32:00 +0800

    script

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -5,7 +5,7 @@
  */
 function plugin_assets_url($path)
 {
-    $plugin_dir = WP_PLUGIN_DIR . '/contact-form-extra-field/assets';
+    $plugin_dir = WP_PLUGIN_DIR . '/contact-form-extra-field-v2/assets';
     if (file_exists($plugin_dir . '/hot')) {
         // var_dump(WP_SITEURL);
         // $url = file_get_contents($plugin_dir . '/hot');
@@ -17,8 +17,8 @@ function plugin_assets_url($path)
     $manifest = json_decode(file_get_contents($manifestPath), true);
     
     if (isset($manifest[$path])) {
-        return WP_CONTENT_URL . '/plugins/contact-form-extra-field/assets' . $manifest[$path];
+        return WP_CONTENT_URL . '/plugins/contact-form-extra-field-v2/assets' . $manifest[$path];
     } else {
-        return WP_CONTENT_URL . '/plugins/contact-form-extra-field/assets' . $path;
+        return WP_CONTENT_URL . '/plugins/contact-form-extra-field-v2/assets' . $path;
     }
 }
