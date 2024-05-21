3d91b539b8fa58dceba969ba423ebb4f1d99022b
RevaARizky <revaathallah86@gmail.com>
2024-05-21 13:35:03 +0800

    v2

--- contact-form-extra-field-v2.php
+++ contact-form-extra-field-v2.php
@@ -3,7 +3,7 @@
  * Plugin Name: Contact Form Extra Field V2
  */
 require_once "includes/main.php";
-require_once "includes/functions.php";
+// require_once "includes/functions.php";
 require_once "includes/nonce.php";
 require_once "includes/template-tags.php";
 require_once "includes/enqueue-assets.php";
